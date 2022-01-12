951928135f1943f107816182c882418699ff1185
Skeef79 <theskeef79@gmail.com>
2022-01-12 07:00:02 +0500

    final

--- app/server.py
+++ app/server.py
@@ -3,7 +3,7 @@ import pika
 from dotenv import load_dotenv, find_dotenv
 import os
 import requests
-import sys
+import time
 
 class RabbitServer:
     def __init__(self):
@@ -13,20 +13,18 @@ class RabbitServer:
         self.password_mq = os.environ.get('RABBITMQ_PASSWORD')
         self.task_queue = os.environ.get('RABBITMQ_TASK_QUEUE')
 
-        self.connection = pika.BlockingConnection(
-            pika.ConnectionParameters(
-                host=self.host_mq, port=self.port_mq, credentials=pika.PlainCredentials(self.user_mq, self.password_mq))
-        )
+        try:
+            self.connection = pika.BlockingConnection(
+                pika.ConnectionParameters(
+                    host=self.host_mq, port=self.port_mq, credentials=pika.PlainCredentials(self.user_mq, self.password_mq))
+            )
 
+            self.channel = self.connection.channel()
+            self.channel.queue_declare(self.task_queue)
 
-        # # versions without credentials
-        # self.connection = pika.BlockingConnection(
-        #     pika.ConnectionParameters(
-        #         host=self.host_mq, port=self.port_mq)
-        # )
-
-        self.channel = self.connection.channel()
-        self.channel.queue_declare(self.task_queue)
+        except Exception as e:
+            print("не удалось подключится к очереди сообщений")
+            print(e)
 
 
     def handle_message(self, ch, method, properties, body):         
@@ -43,24 +41,26 @@ class RabbitServer:
             self.channel.basic_ack(method.delivery_tag)
             return
 
-        flask_url = os.environ.get('Flask_URL')
-        flask_url = 'http://127.0.0.1:5000/' + "make_task"
+        flask_url = os.environ.get('FLASK_URL')
+        flask_url = f'http://{flask_url}/' + "make_task"
 
-        try:
-            #запрос по html к апи фласка.
-            response = requests.post(flask_url, json = {"handle" : message['handle'], "sid" : message['sid']})
+        while True:
+            try:
+                #запрос по html к апи фласка.
+                response = requests.post(flask_url, json = {"handle" : message['handle'], "sid" : message['sid']})
 
-            if response.status_code == requests.codes.ok:
-                print('Ура, задача выполнена и удалена из очереди')
-                self.channel.basic_ack(method.delivery_tag)
-            else:
-                print(f'Что то пошло не так, задача не выполнена. Ответ сервера: {response.status_code}')
+                if response.status_code == requests.codes.ok:
+                    print('Ура, задача выполнена и удалена из очереди')
+                    self.channel.basic_ack(method.delivery_tag)
+                else:
+                    print(f'Что то пошло не так, задача не выполнена. Ответ сервера: {response.status_code}')
+                    self.channel.basic_ack(method.delivery_tag)
 
-        except Exception as e:
-            print("Flask server is not available")
-            print("Код ошибки : {e}")
-
-        return
+                return
+            except Exception as e:
+                print("Flask server is not available")
+                print(f"Код ошибки : {e}")
+            time.sleep(3)
         
     def run_server(self):
         self.channel.basic_consume(
